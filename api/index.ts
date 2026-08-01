371c2f379ff793dfdc7b04e605378b906a65d8ef
Priyanshu Mudgal <priyanshumudgal3@gmail.com>
2026-08-01 22:47:49 +0530

    minor changes

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { app, initializeApp } from "../server";
+import { app, initializeApp } from "../server.ts";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   try {
